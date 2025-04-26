0ac4f35ef7ed9970fc627ffdb11f0d44ac16208f
Deineko Artem <68924024+TheDeinekoArtem@users.noreply.github.com>
2025-04-27 00:49:20 +0300

    Add registration and login functionality with forms
    - Created templates for registration and login pages with form handling and CSRF protection.
    - Added redirects to the home page after successful registration and login.

--- devicemarket/settings.py
+++ devicemarket/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'mssql',
         'NAME': 'OnlineShop',
-        'HOST': 'YourServerName',
+        'HOST': 'localhost',
         'OPTIONS': {
             'driver': 'ODBC Driver 17 for SQL Server',
             'trusted_connection': 'yes',
